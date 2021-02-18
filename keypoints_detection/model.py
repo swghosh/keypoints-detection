c2bd028cf30f5c9893ad01230b08368c56536a1d
Swarup Ghosh <snwg@live.com>
2021-02-18 17:05:31 +0530

    Use crossval in model.fit

--- keypoints_detection/model.py
+++ keypoints_detection/model.py
@@ -9,47 +9,70 @@ dataset_path = '../300w_cropped'
 batch_size = 32
 shuffle_buffer = 16
 epochs = 50
-train_split = 0.7
+train_split = 0.9
 num_samples = 600
-
-base_model = tf.keras.applications.ResNet50V2(include_top=False,
-                                              weights='imagenet',
-                                              input_shape=(image_size, image_size, 3),
-                                              pooling='avg')
-base_model.trainable = False
-
-model = tf.keras.Sequential([
-    base_model,
-    tf.keras.layers.Dense(num_keypoints * keypoints_coords),
-    tf.keras.layers.Reshape([num_keypoints, keypoints_coords])
-])
-model.compile('rmsprop', 'mse')
-
-dataset_gen = dataset.load_dataset_as_generator(
-    dataset_path, image_size)
-ds = tf.data.Dataset.from_generator(lambda: dataset_gen,
-                                    output_signature=(
-                                        tf.TensorSpec(shape=(image_size, image_size, 3),
-                                                      dtype=tf.uint8),
-                                        tf.TensorSpec(shape=(num_keypoints, keypoints_coords),
-                                                      dtype=tf.float32)))
+shuffle_seed = 91
+cross_val_num_folds = 10
 
 
-def preprocess_samples(image, keypoints):
-    image = tf.cast(image, tf.float32)
-    image = tf.keras.applications.resnet_v2.preprocess_input(image)
-    keypoints = keypoints / image_size  # normalize keypoints scaling it by image size
-    return image, keypoints
+def create_model(finetune=False):
+    """Creates a ResNet50V2 model for keypoints detection."""
 
-ds = ds.shuffle(shuffle_buffer)
-ds = ds.map(preprocess_samples, tf.data.experimental.AUTOTUNE)
+    base_model = tf.keras.applications.ResNet50V2(include_top=False,
+                                                  weights='imagenet',
+                                                  input_shape=(image_size, image_size, 3),
+                                                  pooling='avg')
+    base_model.trainable = finetune
 
-train_samples = round(num_samples * train_split)
-train_ds = ds.take(train_samples).cache()
-val_ds = ds.skip(train_samples).cache()
+    model = tf.keras.Sequential([
+        base_model,
+        tf.keras.layers.Dense(num_keypoints * keypoints_coords),
+        tf.keras.layers.Reshape([num_keypoints, keypoints_coords])
+    ])
+    model.compile('rmsprop', 'mse')
 
-train_ds, val_ds = train_ds.batch(batch_size), val_ds.batch(batch_size)
+    return model
 
-model.fit(train_ds,
-          epochs=epochs,
-          validation_data=val_ds)
+
+def prepare_and_get_dataset():
+    """Construct `tf.data.Dataset` pipeline(s) for training and
+    validation splits and return them."""
+
+    ds = tf.data.Dataset.from_generator(lambda: dataset.load_dataset_as_generator(
+                                            dataset_path, image_size),
+                                        output_signature=(
+                                            tf.TensorSpec(shape=(image_size, image_size, 3),
+                                                        dtype=tf.uint8),
+                                            tf.TensorSpec(shape=(num_keypoints, keypoints_coords),
+                                                        dtype=tf.float32)))
+
+    def preprocess_samples(image, keypoints):
+        image = tf.cast(image, tf.float32)
+        image = tf.keras.applications.resnet_v2.preprocess_input(image)
+        keypoints = keypoints / image_size  # normalize keypoints scaling it by image size
+        return image, keypoints
+
+    ds = ds.cache()
+    ds = ds.shuffle(shuffle_buffer, seed=shuffle_seed, reshuffle_each_iteration=False)
+    ds = ds.map(preprocess_samples, tf.data.experimental.AUTOTUNE)
+
+    train_samples = round(num_samples * train_split)
+    train_ds = ds.take(train_samples)
+    val_ds = ds.skip(train_samples)
+
+    train_ds = train_ds.shuffle(
+        shuffle_buffer,
+        reshuffle_each_iteration=True).batch(batch_size)  # additionally, shuffle train samples
+    val_ds = val_ds.batch(batch_size)
+
+    return train_ds, val_ds
+
+
+for _ in range(cross_val_num_folds):
+    tf.keras.backend.clear_session()
+    model = create_model()
+    train_ds, val_ds = prepare_and_get_dataset()
+    model.fit(train_ds,
+              epochs=epochs,
+              validation_data=val_ds)
+    shuffle_seed += 1
